364d8d4a8c23277cc2b08c62735e2d6cc6aaeaae
deepcell <deepcell@gmail.com>
2018-12-12 21:21:49 +0700

    added seed box IP address

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,7 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "66.42.33.40:17236",
-  "prometheus.gq:17236",
+  //"prometheus.gq:17236",
 };
 
 struct CheckpointData {
